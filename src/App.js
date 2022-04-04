7571778fe36d3a8dda7b19e029d39f712192da25
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-04-04 13:38:42 +0200

    counter-with-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counterValue, setCounterValue] = useState(0);
+  const [counterColor, setCounterColor] = useState("#333333");
+
+  function updateCounter(value) {
+    setCounterValue(counterValue + value);
+  }
+
+  useEffect(updateCounterColor, [counterValue]);
+
+  function updateCounterColor() {
+    if (counterValue == 0) {
+      setCounterColor("#333333");
+    } else if (counterValue < 0) {
+      setCounterColor("red");
+    } else {
+      setCounterColor("green");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row max-height align-items-center">
+        <div className="col-10 mx-auto col-md-6 text-center main-container p-5">
+          <h1 className="text-uppercase">counter</h1>
+          <Counter value={counterValue} color={counterColor} />
+          <div className="btn-container d-flex justify-content-around flex-wrap">
+            <Button counterAction={() => updateCounter(-1)} text="Lower count" />
+            <Button counterAction={() => updateCounter(1)} text="Add count" />
+          </div>
+        </div>
+      </div>
     </div>
+  );
+}
+
+function CounterBox() {
+
+}
+
+function Button({ text, counterAction }) {
+  return (
+    <button
+      class="btn counterBtn prevBtn text-uppercase m-2"
+      onClick={counterAction}>
+      {text}
+    </button>
+  );
+}
+
+function Counter({ value, color }) {
+  return (
+    <h1
+      id="counter"
+      style={{ "color": color }}
+    >{value}
+    </h1>
   );
 }
 
